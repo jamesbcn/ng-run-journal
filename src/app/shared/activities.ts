1540488fe659fa8fdffcb61ece41149b70bfa74f
jamesbcn <jamesquinlan@hotmail.co.uk>
2018-04-26 02:02:33 +0200

    package update

--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -2,15 +2,7 @@ import { IActivity } from './activity.model';
 
 export const SAVED_ACTIVITIES: IActivity[] = [
 {
-  "id" : 1,
-  "name" : "Main Bike Trails",
-  "date" : new Date('06/01/2017'),
-  "distance" : 16.2,
-  "comments" : "Nice day, cool temps",
-  "gpxData": '../../assets/gpx/1.gpx'
-},
-{
-   "id": 2,
+   "id": 1,
    "name": 'Industrial Park',
    "date": new Date('06/04/2017'),
    "gpxData": '../../assets/gpx/1.gpx',
@@ -18,7 +10,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
    "comments": 'Cool and windy.'
  },
  {
-   "id": 3,
+   "id": 2,
    "name": 'Forest Route',
    "date": new Date('06/05/2017'),
    "gpxData": '../../assets/gpx/2.gpx',
@@ -26,7 +18,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
    "comments": 'Evening run.'
  },
  {
-   "id": 4,
+   "id": 3,
    "name": 'Lake Shore',
    "date": new Date('06/08/2017'),
    "gpxData": '../../assets/gpx/4.gpx',
