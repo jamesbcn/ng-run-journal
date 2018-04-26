5e8093f7fe623389d8235b610995548c06422a7f
jamesbcn <jamesquinlan@hotmail.co.uk>
2018-04-26 02:33:22 +0200

    environment error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: false,
+  MAPBOX_API_KEY: 'pk.eyJ1IjoiamFtZXNiY24iLCJhIjoiY2pkZHlrdWExMDZvMjJ3bnR6ZHR6cHJ5eSJ9.Wp_CEihCb_e1TC-HyW84JQ'
 };
